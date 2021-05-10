f83020be6e653faa4e43a4fe0800e390d4f05e7f
Travis Newton <travis@newtonnet.net>
2021-05-09 23:56:55 -0400

    still broke -- idk what I'm doing

--- restaurant-9.4.py
+++ restaurant-9.4.py
@@ -1,20 +1,18 @@
 # Exercise 9-4 Number Served
-
 class Restaraunt:
-    def __init__(self, restaurant_name, cusine_type, number_served):
+    def __init__(self, restaurant_name, cusine_type):
         self.restaurant_name = restaurant_name
         self.cusine_type = cusine_type
         self.number_served = 0
 
     def describe_restaurant(self):
         print(f"{self.restaurant_name} is {self.cusine_type}")
-        print(f"{self.restaurant_name} has served {self.number_served} customers.")
-        print("-----------------------------------------------------------------------")
 
-    def set_number_served(self):
+    def set_number_served(self, number_served):
         number_served = self.number_served
 
-restaraunt = Restaraunt("McDonalds", "Fast Food", 100000)
+
+restaraunt = Restaraunt("McDonalds", "Fast Food")
 restaraunt.describe_restaurant()
 
 another_restaraunt = Restaraunt("Chilis", "Fake Mexican or TexMex")
