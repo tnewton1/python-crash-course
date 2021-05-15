087e530a5cbcf68413afa5325cb3dbec955e53a6
Travis Newton <travis@newtonnet.net>
2021-05-14 23:00:55 -0400

    wrapped exercise

--- restaurant-9.4.py
+++ restaurant-9.4.py
@@ -20,5 +20,11 @@ restaraunt.describe_restaurant()
 restaraunt.number_served = 9924
 print(f"\nNumber Served: {restaraunt.number_served}")
 
+restaraunt.set_number_served(18294)
+print("\nNumber Served: " + str(restaraunt.number_served))
+
+restaraunt.increment_number_served(2456)
+print("Number Served: " + str(restaraunt.number_served))
+
 another_restaraunt = Restaraunt("Chilis", "Fake Mexican or TexMex")
 another_restaraunt.describe_restaurant()
