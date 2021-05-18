22f8ba6a83a904167292d1c6b9d5770d83aecd07
Travis Newton <travis@newtonnet.net>
2021-05-18 00:14:23 -0400

    added battery

--- electric_car.py
+++ electric_car.py
@@ -27,6 +27,12 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class"""
         super().__init__(make, model, year)
+        self.battery_size = 75
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size"""
+        print(f"This car has a {self.battery_size}-kWh battery.")
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
