3ca9b4a19cd0977afc52073bc16dd8cfe818aa07
Travis Newton <travis@newtonnet.net>
2021-05-24 23:27:45 -0400

    Instances as attributes

--- electric_car.py
+++ electric_car.py
@@ -26,17 +26,23 @@ class Car:
     def fill_gas_tank(self):
         self.gas_tank_level = 100
         print("The gas tank is now full!")
+
+class Battery:
+    """A simple attempt to model a battery for an electric car"""
+    def __init__(self, battery_size=75):
+        """Initialize the battery size"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
     
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class"""
         super().__init__(make, model, year)
-        self.battery_size = 75
-    
-    def describe_battery(self):
-        """Print a statement describing the battery size"""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+        self.battery = Battery()
 
     def fill_gas_tank(self):
         """Electric cars don't have gas tanks."""
@@ -44,10 +50,5 @@ class ElectricCar(Car):
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
 
-my_chevy = Car("chevy", "cruze", 2015)
-print(my_chevy.get_descriptive_name())
-chevy_fuel = my_chevy.gas_tank_level
-print(f"The fuel level is currently: ", chevy_fuel, "%" )
-my_chevy.fill_gas_tank()
