6ef1f816ed629579ecf8cb183bd99895aa53a0cf
Travis Newton <travis@newtonnet.net>
2021-05-26 21:53:18 -0400

    Added battery range to battery class

--- electric_car.py
+++ electric_car.py
@@ -36,6 +36,15 @@ class Battery:
     def describe_battery(self):
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides"""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} miles on a full charge.")
     
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
@@ -51,4 +60,5 @@ class ElectricCar(Car):
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
 
